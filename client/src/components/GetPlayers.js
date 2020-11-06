a84b350716f7236c0c778b79bbf99a229cc747b6
Vish Singh <46301614+vishalicious213@users.noreply.github.com>
2020-11-06 02:05:57 -0800

    Update GetPlayers.js

--- client/src/components/GetPlayers.js
+++ client/src/components/GetPlayers.js
@@ -55,7 +55,8 @@ class GetPlayers extends React.Component {
         return (
             <section className='toggle'>
                 <header className='top'>
-                    <img className='fifaLogo' src='https://1000logos.net/wp-content/uploads/2017/01/FIFA-Logo-500x381.png' alt='FIFA logo'></img>
+                    // <img className='fifaLogo' src='https://1000logos.net/wp-content/uploads/2017/01/FIFA-Logo-500x381.png' alt='FIFA logo'></img>
+                    <img className='fifaLogo' src='http://felele247.com.ng/wp-content/uploads/2020/04/FIFA-Logo-500x381.png' alt='FIFA logo'></img>
                     <div className='App-header'>
                         <h2 className='title'>Women's World Cup</h2>
                         <div className='subtitle'>2019 Player Search Rankings</div>
